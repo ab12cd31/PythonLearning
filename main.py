bdac402a0bd08214fbe2ee83b65ac538f5596aa8
ab12cd31 <yxiaoshen@163.com>
2022-07-31 12:40:55 +0800

    Python学习

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# 获取星期字符串
-weekStr = "星期一星期二星期三星期四星期五星期六星期日"
-weekId = eval(input("请输入星期数字（1-7）:"))
-pos = (weekId - 1) * 3
-print(weekStr[pos:pos + 3])
+ls = ['p', 'p', 'y', 'y', 123]
+print(ls)
+s = set(ls)
+print(s)
+ls = list(s)
+print(ls)
